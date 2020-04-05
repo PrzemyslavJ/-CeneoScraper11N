8fc1b579185c053d6476a77ae2d7c21407791071
Przemyslaw J <jaworskiprzemyslaw95@gmail.com>
2020-04-05 14:55:14 +0200

    DO 2020.04.05

--- Scraper.py
+++ Scraper.py
@@ -1,38 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
+import json
 
 #pobieranie kodu pojedyńczej strony z opiniami o produkcie
+url_prefix = "http://www.ceneo.pl"
+url_postfix = "/85910996#tab=reviews"
 url = "http://ceneo.pl/85910996#tab=reviews"
-respons = request.get(url)
+respons = requests.get(url)
 page_dom = BeautifulSoup(respons.text,'html.parser')
 
 #wydobycie z kodu strony fragmentów odpowiadających opiniom konsumentów
-opinions = page.dom.select("li.js_product-review")
+opinions = page_dom.select("li.js_product-review")
 
-opinion = opinions.pop(0)
-#dla pojedyńczej opinii wydobycie jej składowych
-opinion_id = opinion(["data-entry-id"])
-author = opinion.select("div.reviewer-name-line").pop().text
-try:
-    recomendation = opinion.select("div.product-review-summary").pop().text
-except IndexError:
-    recomendation = None
-stars = opinion.select("div.product-review-summary").pop().text
-content = opinion.select("p.product-review-body").pop().text
-try:
-    cons = opinion.select().pop().text
-except IndexError:
-    cons = None
-try:
-    pros = opinion.select("div.pros-cell>ul").pop().text
-except IndexError:
-    pros = None
-useful = opinion.select(" button.vote-yes > span").pop().text
-useless = opinion.select("button.vote-no > span").pop().text
-opinion_date = opinion.select("span review-time > time:first-child").pop()["datetime"].text
-try:
-    purchase_date = opinion.select("pan review-time > time:nth-child(2)").pop()["datetime"].text
-except IndexError:
-    purchase_date = None
+all_opinions = []
 
-print(opinion_id,author,recommendation,stars,content)
+while url:
+    # pobranie pojedyńczej strony z opiniami
+    respons = requests.get(url)
+    page_dom = BeautifulSoup(respons.text,'html.parser')
+    #dla pojedyńczej opinii wydobycie jej składowych
+    for opinion in opinions:
+        opinion_id = opinion(["data-entry-id"])
+        author = opinion.select("div.reviewer-name-line").pop().text
+        try:
+            recomendation = opinion.select("div.product-review-summary").pop().text
+        except IndexError:
+            recomendation = None
+        stars = opinion.select("div.product-review-summary").pop().text
+        content = opinion.select("p.product-review-body").pop().text
+        try:
+            cons = opinion.select().pop().text
+        except IndexError:
+            cons = None
+        try:
+            pros = opinion.select("div.pros-cell>ul").pop().text
+        except IndexError:
+            pros = None
+        useful = opinion.select(" button.vote-yes > span").pop().text
+        useless = opinion.select("button.vote-no > span").pop().text
+        opinion_date = opinion.select("span review-time > time:first-child").pop()["datetime"].text
+        try:
+            purchase_date = opinion.select("pan review-time > time:nth-child(2)").pop()["datetime"].text
+        except IndexError:
+            purchase_date = None
+
+        features = {
+            "opinion_id":opinion_id,
+            "author":author,
+            "recommendation":recomendation,
+            "stars":stars,
+            "content":content,
+            "cons":cons,
+            "pros":pros,
+            "useful":useful,
+            "useless":useless,
+            "opinion_date":opinion_date,
+            "purchase_date":purchase_date
+
+        }
+        all_opinions.append(features)
+    try:
+        url = url.prefix+page_dom.select("a.paigination_next").pop()["href"]
+    except IndexError:
+        url = None
+    print(len(all_opinions))
+    print(url)
+
+with open('opinions.json','w',encoding="UTF-8") as fp:
+    json.dump(all_opinions,fp,indent=4, ensure_ascii=False)
+
+url = url.prefix+page_dom.select("a.paigination_next").pop()
+pprint.pprint(features)
